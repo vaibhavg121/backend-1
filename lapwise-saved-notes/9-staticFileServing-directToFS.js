0376be7d8dc1305a2b16c8ac0879a089c54e50cb
Vaibhav <vaibhavg1201@gmail.com>
2022-12-06 15:19:09 +0530

    static serving files-express.static() for CSS files

--- lapwise-saved-notes/9-staticFileServing-directToFS.js
+++ lapwise-saved-notes/9-staticFileServing-directToFS.js
@@ -1,3 +1,11 @@
+//Serving static files.
+//i.e. these files are not handled by express or other middlewares
+//But they are directly forwarded by the fs-module of node.
+
+// *** Using express.static() middleware to serve css files
+
+//Copy-pasting only app.js + Addition of CSS files in public folder + adding classes to HTML files.
+
 //Serving static files with express.static
 
 const path = require("path");
